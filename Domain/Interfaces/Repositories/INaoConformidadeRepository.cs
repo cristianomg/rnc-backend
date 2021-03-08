ad64b35bbe5a432c7e5b9a257b01d531567539e0
Paulo Henrique Medeiros Bittencourt <paulohenriquebitt@gmail.com>
2021-03-08 19:26:57 -0300

    Revert "Utilização do Iqueryable"
    This reverts commit b1cab95c11371c1b701b5ee88e66cf7d6eef42c7.

--- Domain/Interfaces/Repositories/INaoConformidadeRepository.cs
+++ Domain/Interfaces/Repositories/INaoConformidadeRepository.cs
@@ -1,12 +1,11 @@
 ﻿using Domain.Entities;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Domain.Interfaces.Repositories
 {
     public interface INaoConformidadeRepository : IBaseRepository<NaoConformidade>
     {
-        IQueryable<NaoConformidade> GetByTipoNaoConformidade(int tipoNaoConformidadeId);
+        Task<IEnumerable<NaoConformidade>> GetByTipoNaoConformidade(int tipoNaoConformidadeId);
     }
 }
