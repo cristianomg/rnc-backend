690028e0f4fd61e0fe638c6e51a60a3e6a14ac5e
Danilo Michell <danilo.michell@souunit.com.br>
2021-09-11 12:15:54 -0300

    Ajustes

--- Api.Rnc/Controllers/OccurrenceClassificationController.cs
+++ Api.Rnc/Controllers/OccurrenceClassificationController.cs
@@ -1,8 +1,7 @@
-﻿using Api.Rnc.Extensions;
-using AutoMapper;
+﻿using AutoMapper;
 using Domain.Dtos.Inputs;
+using Domain.Dtos.Responses;
 using Domain.Interfaces.Repositories;
-using Domain.Interfaces.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -30,12 +29,12 @@ namespace Api.Rnc.Controllers
         /// <returns></returns>
         [AllowAnonymous]
         [HttpGet]
-        [ProducesResponseType(typeof(IQueryable<DtoSetor>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(IQueryable<DtoOccurrenceClassification>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> GetAllClassifications()
         {
-            var users = await _occurrenceClassificationRepository.GetAllClassification();
-            return Ok(_mapper.ProjectTo<DtoSetor>(users));
+            var classifications = await _occurrenceClassificationRepository.GetAllClassification();
+            return Ok(_mapper.ProjectTo<DtoOccurrenceClassification>(classifications));
         }
     }
 }
