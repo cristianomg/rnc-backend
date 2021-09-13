fdaf4e217b355df1fd660637fd8dd535b18f9d4a
Danilo Michell <danilo.michell@souunit.com.br>
2021-09-13 16:56:04 -0300

    Ajustes

--- Domain/Dtos/Responses/DtoCreateRootCauseAnalysisResponse.cs
+++ Domain/Dtos/Responses/DtoCreateRootCauseAnalysisResponse.cs
@@ -6,8 +6,12 @@ namespace Domain.Dtos.Responses
 {
     public class DtoCreateRootCauseAnalysisResponse
     {
+        public DtoCreateRootCauseAnalysisResponse()
+        {
+            FiveWhat = new List<DtoFiveWhat>();
+        }
         public int NonComplianceRegisterId { get; set; }
-        public DtoFiveWhat FiveWhat { get; set; }
+        public IEnumerable<DtoFiveWhat> FiveWhat { get; set; }
         public int UserId { get; set; }
         public int ActionPlainId { get; set; }
         public virtual IEnumerable<DtoActionPlainResponse> ActionPlainResponses { get; set; }
