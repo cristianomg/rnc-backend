f5c1ff4eba7c5eccf188d20f466febc5b8846092
Danilo Michell Lisboa de Freitas <danilomichell14@gmail.com>
2021-10-16 19:50:20 -0300

    Correção de campos vindo nulo

--- src/4Lab.Orchestrator/Facades/GetOccurrenceRegisterByIdFacade.cs
+++ src/4Lab.Orchestrator/Facades/GetOccurrenceRegisterByIdFacade.cs
@@ -1,4 +1,5 @@
-﻿using _4lab.Occurrences.Application.Service;
+﻿using _4lab.Administration.Application.Service;
+using _4lab.Occurrences.Application.Service;
 using _4Lab.Archives.Application.Service;
 using _4Lab.Core.Enums;
 using _4Lab.Orchestrator.DTOs.Response;
@@ -13,20 +14,24 @@ namespace _4Lab.Orchestrator.Facades
     public class GetOccurrenceRegisterByIdFacade : IGetOccurrenceRegisterByIdFacade
     {
         private readonly IOccurrenceAppService _occurrenceAppService;
+        private readonly IUserAppService _userAppService;
         private readonly IArchiveAppService _archiveAppService;
         private readonly IMapper _mapper;
         public GetOccurrenceRegisterByIdFacade(IOccurrenceAppService occurrenceAppService
                                               , IArchiveAppService archiveAppService
-                                              , IMapper mapper)
+                                              , IMapper mapper
+                                              , IUserAppService userAppService)
         {
             _occurrenceAppService = occurrenceAppService;
             _archiveAppService = archiveAppService;
             _mapper = mapper;
+            _userAppService = userAppService;
         }
         public async Task<DtoOccurrenceRegisterFacadeResponse> Execute(Guid id)
         {
-            var occurrenceRegister = _mapper.Map<DtoOccurrenceRegisterFacadeResponse>
-                (await _occurrenceAppService.GetOccurrenceRegisterById(id));
+            var ocurrence = await _occurrenceAppService.GetOccurrenceRegisterById(id);
+            ocurrence.UserName = _userAppService.GetUserByIdWithInclude(ocurrence.UserId).Result.Name;
+            var occurrenceRegister = _mapper.Map<DtoOccurrenceRegisterFacadeResponse>(ocurrence);
 
             if (occurrenceRegister == null)
                 throw new Exception("Registro de ocorrencia não encontrado.");
@@ -37,7 +42,7 @@ namespace _4Lab.Orchestrator.Facades
                     (await _archiveAppService.GetFilesByEntityId
                             (
                                 EntityArchiveType.Occurrence,
-                                occurrence.Id.Value + "-" + occurrenceRegister.Id 
+                                occurrence.Id.Value + "-" + occurrenceRegister.Id
                             )
                     );
             }
---
+++ src/4Lab.Orchestrator/Interfaces/IGetOccurrenceRegisterAll.cs
@@ -0,0 +1,12 @@
+﻿using _4lab.Occurrences.Application.DTOs;
+using _4Lab.Orchestrator.DTOs.Response;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _4Lab.Orchestrator.Interfaces
+{
+    public interface IGetOccurrenceRegisterAll
+    {
+        Task<IQueryable<DtoOccurrenceRegisterFacadeResponse>> Execute(IQueryable<DtoOccurrenceRegisterResponse> dtoOccurrences);
+    }
+}
