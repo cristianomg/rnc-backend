1fdd59f82c8a1b2c20a6c7db431f0abe109aaad5
paulo henrique <paulotombit@hotmail.com>
2021-10-31 20:43:13 -0300

    Criação de automaper

---
+++ 4Lab.Satisfaction.Application/Mapper/SatisfactionMapper.cs
@@ -0,0 +1,48 @@
+﻿using _4Lab.Satisfaction.Application.DTOs;
+using _4Lab.Satisfaction.Domain.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4Lab.Satisfaction.Application.Mapper
+{
+    public class SatisfactionMapper : Profile
+    {
+        public SatisfactionMapper() 
+        {
+            CreateMap<DeliveryResults, DtoDeliveryResults>()
+                .ReverseMap();
+
+            CreateMap<HowSatisfied, DtoHowSatisfied>()
+                .ReverseMap();
+
+            CreateMap<OurDifferential, DtoOurDifferential>()
+                .ReverseMap();
+
+            CreateMap<OverallImpression, OverallImpression>()
+                .ReverseMap();
+
+            CreateMap<PersonalInformations, DtoPersonalInformation>()
+                .ReverseMap();
+
+            CreateMap<Reception, DtoReception>()
+                .ReverseMap();
+
+            CreateMap<Sanitation, DtoPersonalInformation>()
+                .ReverseMap();
+
+            CreateMap<TecnicalArea, DtoTecnicalArea>()
+                .ReverseMap();
+
+            CreateMap<WhySearch, DtoWhySearch>()
+                .ReverseMap();
+
+            CreateMap<SatisfactionSurvey, DtoSatisfactionSurvey>()
+                .ReverseMap();
+
+        }
+    }
+}
