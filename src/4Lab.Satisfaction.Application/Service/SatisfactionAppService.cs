415769621784f90c9e4a712ce5374459cdf60e30
Danilo Michell Lisboa de Freitas <danilomichell14@gmail.com>
2021-10-31 20:02:50 -0300

    Ajustes

--- src/4Lab.Satisfaction.Application/Service/SatisfactionAppService.cs
+++ src/4Lab.Satisfaction.Application/Service/SatisfactionAppService.cs
@@ -11,42 +11,28 @@ namespace _4Lab.Satisfaction.Application.Service
     public class SatisfactionAppService : ISatisfactionAppService
     {
         private readonly IMapper _mapper;
-        //private readonly ISatisfactionRepository _satisfactionRepository;
-        public SatisfactionAppService(IMapper mapper)
+        private readonly ISatisfactionRepository _satisfactionRepository;
+        public SatisfactionAppService(IMapper mapper, ISatisfactionRepository satisfactionRepository)
         {
-            //_satisfactionRepository = satisfactionRepository;
+            _satisfactionRepository = satisfactionRepository;
             _mapper = mapper;
         }
 
-        public Task<IQueryable<DtoSatisfactionSurveyResponse>> GetSatisfactionSurveyAll()
+        public async Task<DtoSatisfactionSurveyInput> RegisterSatisfactionSurvey(SatisfactionSurvey satisfactionSurvey)
         {
-            throw new NotImplementedException();
+            var result = await _satisfactionRepository.Insert(satisfactionSurvey);
+            return _mapper.Map<DtoSatisfactionSurveyInput>(result);
         }
 
-        public Task<DtoSatisfactionSurveyResponse> GetSatisfactionSurveyById(Guid id)
+        public async Task<DtoSatisfactionSurveyResponse> GetSatisfactionSurveyById(Guid id)
         {
-            throw new NotImplementedException();
+            var result = await _satisfactionRepository.GetSatisfactionSurveyById(id);
+            return _mapper.Map<DtoSatisfactionSurveyResponse>(result);
         }
-
-        public Task<DtoSatisfactionSurveyInput> RegisterSatisfactionSurvey(SatisfactionSurvey satisfactionSurvey)
+        public async Task<IQueryable<DtoSatisfactionSurveyResponse>> GetSatisfactionSurveyAll()
         {
-            throw new NotImplementedException();
+            var result = await _satisfactionRepository.GetSatisfactionSurveyAll();
+            return _mapper.Map<IQueryable<DtoSatisfactionSurveyResponse>>(result);
         }
-        //public async Task<DtoSatisfactionSurveyInput> RegisterSatisfactionSurvey(SatisfactionSurvey satisfactionSurvey)
-        //{
-        //    var result = await _satisfactionRepository.Insert(satisfactionSurvey);
-        //    return _mapper.Map<DtoSatisfactionSurveyInput>(result);
-        //}
-
-        //public async Task<DtoSatisfactionSurveyResponse> GetSatisfactionSurveyById(Guid id)
-        //{ 
-        //    var result = await _satisfactionRepository.GetSatisfactionSurveyById(id);
-        //    return _mapper.Map<DtoSatisfactionSurveyResponse>(result);
-        //}
-        //public async Task<IQueryable<DtoSatisfactionSurveyResponse>> GetSatisfactionSurveyAll()
-        //{
-        //    var result = await _satisfactionRepository.GetSatisfactionSurveyAll();
-        //    return _mapper.Map<IQueryable<DtoSatisfactionSurveyResponse>>(result);
-        //}
     }
 }
